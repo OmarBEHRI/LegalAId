67270fc4ce1be8619cebe346c3aefc112e3c830d
Omar Behri <omarbehri1@gmail.com>
2024-02-18 07:50:09 +0100

    made progress

--- chat-script.js
+++ chat-script.js
@@ -5,6 +5,8 @@ document.getElementById("chat-input").addEventListener("keypress", function(even
     }
 });
 
+let role = 0;
+
 function sendMessage() {
     // Get the input value
     var message = document.getElementById("chat-input").value;
@@ -22,19 +24,39 @@ function sendMessage() {
     var paragraph = document.createElement("p");
     // Set the id to 'message'
     paragraph.className = "message";
+
+    var iconContainer = document.createElement("div");
+    iconContainer.className = "icon-container";
     // Set the text content to the message
     paragraph.textContent = message;
+     if (role % 2 == 0){
+        var userImage = document.createElement("img");
+        iconContainer.appendChild(userImage);
+        userImage.src = "./user-image.webp";
+        userImage.className = "img-icon";
+     }
+     else{
+        var botImage = document.createElement("img");
+        iconContainer.appendChild(botImage);
+        botImage.src = "./bot-image.svg";
+        botImage.className = "img-icon";
+     }
+
+     messageBox.appendChild(iconContainer);
+
+
+    messageBox.appendChild(paragraph);
 
     // Get the chat content container
     var chatContent = document.getElementById("chat-content");
     // Append the new paragraph to the container
     if (chatContent.firstChild){
-        chatContent.insertBefore(paragraph, chatContent.firstChild);
+        chatContent.insertBefore(messageBox, chatContent.firstChild);
     }
     else{
-        chatContent.appendChild(paragraph);
+        chatContent.appendChild(messageBox);
     }
-
+    role++;
     // Clear the input field after sending the message
     document.getElementById("chat-input").value = "";
 }
